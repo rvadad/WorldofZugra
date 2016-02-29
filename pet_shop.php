5227eb191a525b4661318f4621cfd146a8d32f7a
David Hiett <d_hiett@hotmail.com>
2016-02-29 15:34:05 -0500

    Update pet_shop.php

--- pet_shop.php
+++ pet_shop.php
@@ -1,1 +1,9 @@
+import head.php
+<!-- This file will be linked to by a button in the game's Inn at any town. As I am attempting to make the entire pet system modular and capable of either being present or not and still have the remainder of the game function the same I felt this was the best place to start for now. Likely this will be a lengthy process as I am constantly a perpetual perfectionist expecting that things should continually become better until the point that they do not become invinted at all (see also, babage analytical engine)-->
+<h1>Welcome to the Zugra petshop</h1><br>
+<h4>A place where adorable pets can be adopted by the heros and adventurers in Zugra. A pet will be a fabulous companion to keep you company and will help you in epic battles when you decide to bite off a bit more than you are capable of masticating.</h4><br>
+<a href="pet_adopt.php">Would You like to adopt a pet today?</a><br><!-- We have a button that takes us to the adoption page-->
+<a href="pet_revive.php">Do you need me to revive a dead pet?</a><br><!-- We have a button that takes us to the revive page-->
 
+<!-- What other information could be contained on this page? Imagine it as a pet shop, Can we fill it with caged beasts and a NPC that seems to appreciate the company of beasts as opposed to humanoids? Rudimentary run on a MVP so a lot of work is needed here obviously-->
+import footer.php
