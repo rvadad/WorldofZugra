8133cbb34d0ab5ad620b03ab310ed321faf67fc1
David Hiett <d_hiett@hotmail.com>
2016-03-01 11:46:24 -0500

    Update pet_revive.php

--- pet_revive.php
+++ pet_revive.php
@@ -1,3 +1,13 @@
 <h1>Pet Healer</h1><br>
 <h4>For a price of X gold I can revive your dead pet</h4><br>
 <!--on submit update pet health to max health where 'petname'==$SESSION['petname']-->
+<!--
+two ways to handle this, maxhp field located in the database:
+or
+a list of possible pets and if statements then some equation to determine max hp:
+if pet1
+  maxhp=20+(petxp/1000)
+elseif pet2
+  maxhp=22+(petxp/1005)
+  etc..etc.. or something like that
+-->
