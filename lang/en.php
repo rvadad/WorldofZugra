676f8d7edfb4491f84b41a4f20339c9b9c3345b0
David Hiett <d_hiett@hotmail.com>
2016-03-03 20:07:04 -0500

    Create en.php
    this is the snippet that will get inserted into the language file of the game... We will be needing a translation for the files fr.php de.php it.php etc.

---
+++ lang/en.php
@@ -0,0 +1,10 @@
+<?PHP
+//Pet Language
+
+$lang_pets["battle"] = "triumphantly marches into battle against your opponent";
+$lang_pets["active"] = "Active";
+$lang_pets["name"] = "What a wonderful name for your pet.";
+$lang_pets["dead"] = "Most unfortunately your pet has died... Better take him to the pet shop healer.";
+$lang_pets["revived"] = "Your pet is alive!";
+$lang_pets["new_pets"] = "Your new pet is a:";
+?>
