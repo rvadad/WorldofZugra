41fabc4871982782d970c78fece3e3f17c3615b2
David Hiett <d_hiett@hotmail.com>
2016-03-06 00:33:40 -0500

    Create join_team.php

---
+++ join_team.php
@@ -0,0 +1,11 @@
+<-- 
+Let us now create a page that uses a form to allow people to join the team by putting in 
+appropriate contact information.
+-->
+<?PHP
+?>
+<h2>Join Our Development Team Here</h2>
+
+<table>
+<tr><td>TODO: Build Form Here
+</td></tr></table>
